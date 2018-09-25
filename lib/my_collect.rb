1d19edd050be8813e1a7895e07c1121790f3e7e4
Paul Fell <swellpf@gmail.com>
2018-09-25 15:17:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,6 +2,7 @@ require 'pry'
 def my_collect(collection)
   index = 0
   new_collection = []
+  binding.pry
   while index < collection.length
     new_collection << yield collection[index]
   index +=1
