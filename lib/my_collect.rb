ebf168971ee5feee2b8cdaa2f112ac3e9db44d8f
Paul Fell <swellpf@gmail.com>
2018-09-25 15:15:26 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(collection)
   index = 0
   new_collection = []
   while index < collection.length
-   # yield collection[index]
-    new_collection << yield collection[index]
+    yield collection[index]
+   # new_collection << yield collection[index]
   index +=1
   end
   new_collection
